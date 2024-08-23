912150518e71dc9b63db983fcadd1c48ed569a90
codepath <118053614+javier1234567890qw@users.noreply.github.com>
2024-08-23 13:13:56 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+<<<<<<< HEAD
+=======
+import './index.css'
+>>>>>>> 111e1916a590c009ece9eaf6501ea2c69db9e185
+
+createRoot(document.getElementById('root')).render(
+ 
+    <App />
+  
+)
---
+++ src/routes/Dashboard.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import { Inicio } from "../components/Inicio/Inicio"
+
+export const Dashboard = () => {
+  return (
+    <>
+    
+    
+    <Routes>
+        <Route path="/" element={<Inicio/>} />
+    
+        
+    </Routes>
+    
+    
+    </>
+  )
+}
